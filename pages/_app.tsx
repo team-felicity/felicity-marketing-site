78be84afdac083ae1ea550111ace17cb42880def
Orlan Quijada <orlantquijada@gmail.com>
2021-08-10 18:01:00 +0800

    setup stitches to work with next js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { globalStyles } from '@config/stitches'
+
 function MyApp({ Component, pageProps }: AppProps) {
+  globalStyles() // compiles stitches global styles
+
   return <Component {...pageProps} />
 }
 export default MyApp

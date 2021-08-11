b203473e0efe52a4c9ef73242b88695ce47f6dee
Orlan Quijada <orlantquijada@gmail.com>
2021-08-11 16:24:13 +0800

    added react-id to ensure stable ids between server and client while server-side rendering

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import type { AppProps } from 'next/app'
+import { IdProvider } from '@radix-ui/react-id'
 
 import { globalStyles } from '@config/stitches'
 
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles() // compiles stitches global styles
 
-  return <Component {...pageProps} />
+  return (
+    <IdProvider>
+      <Component {...pageProps} />
+    </IdProvider>
+  )
 }
 export default MyApp

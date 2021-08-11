e1981bda71e355f3fbe79c5c80c972d06498c858
Orlan Quijada <orlantquijada@gmail.com>
2021-08-11 17:58:16 +0800

    implemented Container component

---
+++ components/Container.tsx
@@ -0,0 +1,28 @@
+import { motion } from 'framer-motion'
+
+import { styled } from '@config/stitches'
+
+export default styled(motion.div, {
+  mx: 'auto',
+  px: '$5',
+
+  variants: {
+    size: {
+      '1': {
+        maxWidth: '430px',
+      },
+      '2': {
+        maxWidth: '715px',
+      },
+      '3': {
+        maxWidth: '1145px',
+      },
+      '4': {
+        maxWidth: 'none',
+      },
+    },
+  },
+  defaultVariants: {
+    size: '4',
+  },
+})

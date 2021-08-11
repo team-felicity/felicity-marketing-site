28084828ad92ebb41c75250bf54d9eca7bde32d2
Orlan Quijada <orlantquijada@gmail.com>
2021-08-11 22:36:33 +0800

    added font weights to props

--- components/Text.ts
+++ components/Text.ts
@@ -1,3 +1,5 @@
+import { motion } from 'framer-motion'
+
 import {
   styled,
   mapThemeToCSSProp,
@@ -5,23 +7,29 @@ import {
   CSSProps,
 } from '@config/stitches'
 
-const fontSizeMapKey: CSSProps = 'fontSize'
+const fontSizesMapKey: CSSProps = 'fontSize'
 const colorsMapKey: CSSProps = 'color'
+const fontWeightsMapKey: CSSProps = 'fontWeight'
 
-const fontSizeMap = mapThemeToCSSProp(fontSizeMapKey) as KeysToPropMap<
-  typeof fontSizeMapKey
+const fontSizeMap = mapThemeToCSSProp(fontSizesMapKey) as KeysToPropMap<
+  typeof fontSizesMapKey
 >
 const colorsMap = mapThemeToCSSProp(colorsMapKey) as KeysToPropMap<
   typeof colorsMapKey
 >
+const fontWeightsMap = mapThemeToCSSProp(fontWeightsMapKey) as KeysToPropMap<
+  typeof fontWeightsMapKey
+>
 
-export default styled('span', {
+export default styled(motion.span, {
   variants: {
     size: fontSizeMap,
     color: colorsMap,
+    weight: fontWeightsMap,
   },
   defaultVariants: {
     size: '3',
     color: 'black1',
+    weight: 'regular',
   },
 })

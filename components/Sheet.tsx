13acbb8525048772a51e460a38374ceac9a2653c
Orlan Quijada <orlantquijada@gmail.com>
2021-08-11 17:59:50 +0800

    implemented Sheet component

---
+++ components/Sheet.tsx
@@ -0,0 +1,147 @@
+import { forwardRef, ComponentProps } from 'react'
+import { XIcon } from '@heroicons/react/outline'
+
+import * as Dialog from '@radix-ui/react-dialog'
+import type * as Polymorphic from '@radix-ui/react-polymorphic'
+
+import { styled, keyframes } from '@config/stitches'
+import { Button } from '@components'
+
+const fadeIn = keyframes({
+  from: { opacity: '0' },
+  to: { opacity: '1' },
+})
+
+const fadeOut = keyframes({
+  from: { opacity: '1' },
+  to: { opacity: '0' },
+})
+
+const StyledOverlay = styled(Dialog.Overlay, {
+  backgroundColor: 'rgba(0, 0, 0, .34)',
+  position: 'fixed',
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0,
+  outline: 'none',
+
+  '&[data-state="open"]': {
+    animation: `${fadeIn} 150ms cubic-bezier(0.22, 1, 0.36, 1)`,
+  },
+
+  '&[data-state="closed"]': {
+    animation: `${fadeOut} 150ms cubic-bezier(0.22, 1, 0.36, 1)`,
+  },
+})
+
+// requires children
+type SheetRootProps = ComponentProps<typeof Dialog.Root> & {
+  children: React.ReactNode
+}
+
+export function Root({ children, ...rest }: SheetRootProps) {
+  return (
+    <Dialog.Root {...rest}>
+      <StyledOverlay />
+      {children}
+    </Dialog.Root>
+  )
+}
+
+const slideIn = keyframes({
+  from: { transform: '$$transformValue' },
+  to: { transform: 'translate3d(0,0,0)' },
+})
+
+const slideOut = keyframes({
+  from: { transform: 'translate3d(0,0,0)' },
+  to: { transform: '$$transformValue' },
+})
+
+const StyledContent = styled(Dialog.Content, {
+  backgroundColor: '$white1',
+  // TODO: ask if it looks good with shadow
+  boxShadow:
+    '0 0 38px -10px $colors$shadowLight, 0 0 35px -15px $colors$shadowDark ',
+  position: 'fixed',
+  // stretches left and right side vertically
+  bottom: 0,
+  top: 0,
+
+  '&[data-state="open"]': {
+    animation: `${slideIn} 150ms cubic-bezier(0.22, 1, 0.36, 1)`,
+  },
+
+  '&[data-state="closed"]': {
+    animation: `${slideOut} 150ms cubic-bezier(0.22, 1, 0.36, 1)`,
+  },
+
+  variants: {
+    side: {
+      top: {
+        $$transformValue: 'translate(0,-100%)',
+        width: '100%',
+        height: '300px',
+        bottom: 'auto',
+      },
+      right: {
+        $$transformValue: 'translate(100%,0)',
+        width: '250px',
+        right: 0,
+      },
+      bottom: {
+        $$transformValue: 'translate(0,100%)',
+        width: '100%',
+        height: '300px',
+        top: 'auto',
+      },
+      left: {
+        $$transformValue: 'translate(-100%,0)',
+        width: '250px',
+        left: 0,
+      },
+    },
+  },
+
+  defaultVariants: {
+    side: 'right',
+  },
+})
+
+const StyledCloseButton = styled(Button, {
+  position: 'absolute',
+  top: '$2',
+  right: '$2',
+  cursor: 'pointer',
+  padding: '0.25rem',
+  height: 'fit-content !important',
+  width: 'fit-content',
+  color: '$gray3',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+})
+
+type PolymorphicDialogContent = Polymorphic.ForwardRefComponent<
+  Polymorphic.IntrinsicElement<typeof Dialog.Content>,
+  Polymorphic.OwnProps<typeof StyledContent>
+>
+
+export const Content = forwardRef(({ children, ...rest }, ref) => (
+  <StyledContent {...rest} ref={ref}>
+    {children}
+    <Dialog.Close
+      as={StyledCloseButton}
+      variant="ghost"
+      whileTap={{ filter: 'brightness(0.5)' }}
+    >
+      <XIcon width="1rem" />
+    </Dialog.Close>
+  </StyledContent>
+)) as PolymorphicDialogContent
+
+export const Trigger = Dialog.Trigger
+export const Close = Dialog.Close
+export const Title = Dialog.Title
+export const Description = Dialog.Description
---
+++ components/index.ts
@@ -0,0 +1,9 @@
+export { default as Text } from './Text'
+export { default as Button } from './Button'
+export { default as Link } from './Link'
+export { default as View } from './View'
+export { default as Flex } from './Flex'
+export { default as Grid } from './Grid'
+export { default as Container } from './Container'
+
+export * as Sheet from './Sheet'

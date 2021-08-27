102b46c207638801992079f90f98a762ed0d801e
Orlan Quijada <orlantquijada@gmail.com>
2021-08-27 19:24:38 +0800

    replaced view to container

--- components/Landing/Maps.tsx
+++ components/Landing/Maps.tsx
@@ -1,10 +1,10 @@
-import { View } from '@components'
+import { Container } from '@components'
 
 export default function MapSection() {
   return (
-    <View as="section">
+    <Container as="section" size="large2" css={{ width: '100%' }}>
       <CPIMapIFrame />
-    </View>
+    </Container>
   )
 }
 
@@ -13,7 +13,7 @@ function CPIMapIFrame() {
     <iframe
       width="100%"
       height="450"
-      style={{ border: 0 }}
+      style={{ border: 0, borderRadius: 40 }}
       loading="lazy"
       allowFullScreen
       src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJB_xZQtGYqTMRNPY1g6C2hgg&key=${process.env.NEXT_MAPS_API_KEY}`}

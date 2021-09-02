821c7c623a120c855ef62ff04b388d27f91610ee
Orlan Quijada <orlantquijada@gmail.com>
2021-09-02 16:51:36 +0800

    background fix

--- components/ContactUs/ContactUsSection.tsx
+++ components/ContactUs/ContactUsSection.tsx
@@ -6,7 +6,7 @@ import Maps from './Maps'
 
 export default function ContactUsSection() {
   return (
-    <View as="section" css={{ py: '$9' }}>
+    <View as="section" css={{ py: '$9', backgroundColor: '$white1' }}>
       <Container
         size="large2"
         as={Grid}

d33214c03cc5126125c0ad859c18c74791d32c5a
Orlan Quijada <orlantquijada@gmail.com>
2021-09-07 23:30:52 +0800

    custom fetch wrapper (axios alternative)

---
+++ utils/fetcher.ts
@@ -0,0 +1,38 @@
+const defaultResolver = async (res: Response) => {
+  if (res.ok) return await res.json()
+
+  const err = await res.text()
+  return Promise.reject(new Error(err))
+}
+
+export function gqlClient(
+  query: string,
+  variables: Record<string, unknown> = {}
+) {
+  return fetch(`${process.env.NEXT_PUBLIC_API_URL}/graphql`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  }).then(defaultResolver)
+}
+
+export function restClient(
+  endpoint: string,
+  { body, ...config }: RequestInit = {}
+) {
+  const finalConfig: RequestInit = {
+    method: body ? 'POST' : 'GET',
+    ...config,
+    headers: {
+      'Content-Type': 'application/json',
+      ...config.headers,
+    },
+  }
+
+  return fetch(endpoint, finalConfig).then(defaultResolver)
+}

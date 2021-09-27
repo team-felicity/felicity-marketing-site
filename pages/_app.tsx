c92fb1fd8641997f7f541203991dfb757c5f3e77
Orlan Quijada <orlantquijada@gmail.com>
2021-09-27 15:16:35 +0800

    feat: messenger chat component config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,12 @@ import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import { IdProvider } from '@radix-ui/react-id'
+import { LazyMotion } from 'framer-motion'
+import MessengerCustomerChat from 'react-messenger-customer-chat'
 
 import { globalStyles } from '@config/stitches'
 
 import Layout from '@components/Layout'
-import { LazyMotion } from 'framer-motion'
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -29,6 +30,10 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
       <LazyMotion features={loadFeatures}>
         {getLayout(<Component {...pageProps} />)}
       </LazyMotion>
+      <MessengerCustomerChat
+        pageId={process.env.NEXT_PUBLIC_MESSENGER_PAGE_ID}
+        appId={process.env.NEXT_PUBLIC_MESSENGER_APP_ID}
+      />
     </IdProvider>
   )
 }

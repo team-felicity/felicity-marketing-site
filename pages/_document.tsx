744113dafe8d56ed8ebcad1d0a191d6158f5e8f4
Orlan Quijada <orlantquijada@gmail.com>
2021-10-01 10:44:10 +0800

    Revert "try"
    This reverts commit ae032b069160347a3d2359c4d05762a1845e543d.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,32 +15,6 @@ export default class MyDocument extends Document {
             id="stitches"
             dangerouslySetInnerHTML={{ __html: getCssText() }}
           />
-
-          <script
-            id="messenger-chat"
-            dangerouslySetInnerHTML={{
-              __html: `
-            var chatbox = document.getElementById('fb-customer-chat');
-            chatbox.setAttribute("page_id", "${process.env.NEXT_PUBLIC_MESSENGER_PAGE_ID}");
-            chatbox.setAttribute("attribution", "biz_inbox");
-
-            window.fbAsyncInit = function() {
-              FB.init({
-                xfbml            : true,
-                version          : 'v12.0'
-              });
-            };
-
-            (function(d, s, id) {
-              var js, fjs = d.getElementsByTagName(s)[0];
-              if (d.getElementById(id)) return;
-              js = d.createElement(s); js.id = id;
-              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-              fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
-          `,
-            }}
-          />
         </Head>
         <body>
           <Main />

ae032b069160347a3d2359c4d05762a1845e543d
Orlan Quijada <orlantquijada@gmail.com>
2021-10-01 10:31:34 +0800

    try

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 
-import Script from 'next/script'
 import { IdProvider } from '@radix-ui/react-id'
 import { LazyMotion } from 'framer-motion'
 
@@ -33,33 +32,6 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
           {getLayout(<Component {...pageProps} />)}
         </LazyMotion>
       </IdProvider>
-
-      <Script
-        id="messenger-chat"
-        strategy="lazyOnload"
-        dangerouslySetInnerHTML={{
-          __html: `
-            var chatbox = document.getElementById('fb-customer-chat');
-            chatbox.setAttribute("page_id", "${process.env.NEXT_PUBLIC_MESSENGER_PAGE_ID}");
-            chatbox.setAttribute("attribution", "biz_inbox");
-
-            window.fbAsyncInit = function() {
-              FB.init({
-                xfbml            : true,
-                version          : 'v12.0'
-              });
-            };
-
-            (function(d, s, id) {
-              var js, fjs = d.getElementsByTagName(s)[0];
-              if (d.getElementById(id)) return;
-              js = d.createElement(s); js.id = id;
-              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-              fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
-          `,
-        }}
-      />
     </>
   )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 import { getCssText } from '@config/stitches'
@@ -14,6 +15,33 @@ export default class MyDocument extends Document {
           <style
             id="stitches"
             dangerouslySetInnerHTML={{ __html: getCssText() }}
+          />
+
+          <Script
+            id="messenger-chat"
+            strategy="lazyOnload"
+            dangerouslySetInnerHTML={{
+              __html: `
+            var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "${process.env.NEXT_PUBLIC_MESSENGER_PAGE_ID}");
+            chatbox.setAttribute("attribution", "biz_inbox");
+
+            window.fbAsyncInit = function() {
+              FB.init({
+                xfbml            : true,
+                version          : 'v12.0'
+              });
+            };
+
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+          `,
+            }}
           />
         </Head>
         <body>

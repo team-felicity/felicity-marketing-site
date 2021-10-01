118fccbde568eac8c57092fc5e7a5b9b0c8b4ea4
Orlan Quijada <orlantquijada@gmail.com>
2021-10-01 08:53:21 +0800

    fb provided code snippet trial

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import MessengerCustomerChat from 'react-messenger-customer-chat'
 
 import type { ReactNode } from 'react'
 
@@ -25,11 +24,6 @@ export default function Layout({ children, hideFooter = false }: Props) {
         <Header />
         <View as="main" css={{ flexGrow: 1, backgroundColor: '$white1' }}>
           {children}
-
-          <MessengerCustomerChat
-            pageId={process.env.NEXT_PUBLIC_MESSENGER_PAGE_ID}
-            appId={process.env.NEXT_PUBLIC_MESSENGER_APP_ID}
-          />
         </View>
         {hideFooter ? null : <Footer />}
       </Flex>

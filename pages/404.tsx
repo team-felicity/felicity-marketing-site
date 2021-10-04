5506384413729fb3bb6501d50e5a12d7d5f12069
Orlan Quijada <orlantquijada@gmail.com>
2021-10-04 09:39:08 +0800

    hotfix: height issue on 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,14 @@
+import type { ReactElement } from 'react'
+
 import ErrorScreen from '@components/ErrorScreen'
+import Layout from '@components/Layout'
 
 export default function Custom404() {
   return <ErrorScreen />
 }
+
+Custom404.getLayout = (page: ReactElement) => (
+  <Layout css={{ minHeight: '100vh' }} mainContentcss={{ display: 'grid' }}>
+    {page}
+  </Layout>
+)
